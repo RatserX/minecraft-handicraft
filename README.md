f1a3da737bd597ca106e738f44655a249ffa9abd
Miguel <12604047+RatserX@users.noreply.github.com>
2021-01-05 12:34:11 -0500

    refactor: parser

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 [![python-39-blue](https://img.shields.io/badge/python-v3.9-blue)](https://www.python.org/)
 [![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
-[![license-mit-green](https://img.shields.io/badge/license-MIT-green)](https://github.com/RatserX/minecraft-instance-parse/blob/main/LICENSE)
+[![license-mit-green](https://img.shields.io/badge/license-MIT-green)](https://github.com/RatserX/minecraft-instance-parser/blob/main/LICENSE)
 
 [![Buy me a coffee](https://www.buymeacoffee.com/assets/img/guidelines/download-assets-sm-2.svg)](https://www.buymeacoffee.com/Ratser)
 
@@ -12,14 +12,14 @@ Parser for Minecraft instance profiles. It reads and processes profiles and allo
 
 Multiple profiles and be configured and they can be retrieved either from a local directory or a web location. It allows the selection of a download directory, creating the appropiate directories if the path does not exists.
 
-![Minecraft Instance Parse](https://raw.githubusercontent.com/RatserX/ratserx.github.io/master/public/images/minecraft-instance-parse.gif)
+![Minecraft Instance Parser](https://raw.githubusercontent.com/RatserX/ratserx.github.io/master/public/images/minecraft-instance-parser.gif)
 
 ## Prerequisites
 
 * [Python 3.9+](https://www.python.org/ftp/python/3.9.1/python-3.9.1-amd64.exe) - Python is an interpreted, high-level and general-purpose programming language.
 
 ## Install
-1. [Download](https://github.com/RatserX/minecraft-instance-parse/archive/main.zip) the main project.
+1. [Download](https://github.com/RatserX/minecraft-instance-parser/archive/main.zip) the main project.
 2. Extract the package.
 3. (Automatic) Run either `main.bat` (Windows) or `main.sh` (Linux).
 3. (Manual) Install the required packages.
--- src/parser.py
+++ src/parser.py
@@ -7,83 +7,83 @@ import typing
 
 import helper
 
-class ParseProgress:
+class ParserProgress:
     def __init__(self, message: typing.Union[list[str], None], state: str):
         self.message: typing.Union[list[str], None] = message
         self.state: str = state
 
-class ParseState:
+class ParserState:
     # Critical
-    VALIDATE_CRITICAL_ADDON_DOWNLOAD_PROGRESS: str = "VALIDATE_CRITICAL_ADDON_DOWNLOAD_PROGRESS"
-    VALIDATE_CRITICAL_LOADER_DOWNLOAD_PROGRESS: str = "VALIDATE_CRITICAL_LOADER_DOWNLOAD_PROGRESS"
+    PARSER_CRITICAL_ADDON_DOWNLOAD_PROGRESS: str = "PARSER_CRITICAL_ADDON_DOWNLOAD_PROGRESS"
+    PARSER_CRITICAL_LOADER_DOWNLOAD_PROGRESS: str = "PARSER_CRITICAL_LOADER_DOWNLOAD_PROGRESS"
 
     # Info
-    VALIDATE_INFO_ADDON_INSTALL_DRAFT: str = "VALIDATE_INFO_ADDON_INSTALL_DRAFT"
-    VALIDATE_INFO_ADDON_INSTALL_OPTION: str = "VALIDATE_INFO_ADDON_INSTALL_OPTION"
-    VALIDATE_INFO_INSTANCE_INSTALL_PATH: str = "VALIDATE_INFO_INSTANCE_INSTALL_PATH"
-    VALIDATE_INFO_LOADER_INSTALL_DRAFT: str = "VALIDATE_INFO_LOADER_INSTALL_DRAFT"
-    VALIDATE_INFO_LOADER_INSTALL_OPTION: str = "VALIDATE_INFO_LOADER_INSTALL_OPTION"
+    PARSER_INFO_ADDON_INSTALL_DRAFT: str = "PARSER_INFO_ADDON_INSTALL_DRAFT"
+    PARSER_INFO_ADDON_INSTALL_OPTION: str = "PARSER_INFO_ADDON_INSTALL_OPTION"
+    PARSER_INFO_INSTANCE_INSTALL_PATH: str = "PARSER_INFO_INSTANCE_INSTALL_PATH"
+    PARSER_INFO_LOADER_INSTALL_DRAFT: str = "PARSER_INFO_LOADER_INSTALL_DRAFT"
+    PARSER_INFO_LOADER_INSTALL_OPTION: str = "PARSER_INFO_LOADER_INSTALL_OPTION"
     
     # Warn
-    VALIDATE_WARN_ADDON_INSTALL_SKIP: str = "VALIDATE_WARN_ADDON_INSTALL_SKIP"
-    VALIDATE_WARN_LOADER_INSTALL_SKIP: str = "VALIDATE_WARN_LOADER_INSTALL_SKIP"
+    PARSER_WARN_ADDON_INSTALL_SKIP: str = "PARSER_WARN_ADDON_INSTALL_SKIP"
+    PARSER_WARN_LOADER_INSTALL_SKIP: str = "PARSER_WARN_LOADER_INSTALL_SKIP"
 
 class Parse:
-    def __init__(self, parse_option: dict):
-        self.parse_option: dict = parse_option
+    def __init__(self, parser_option: dict):
+        self.parser_option: dict = parser_option
 
-    def download(self, parse_progress_callback: collections.abc.Callable[[ParseProgress], None] = None) -> None:
-        install_path: str = self.parse_option["installPath"]
-        name: str = self.parse_option["name"]
-        installed_addons: list[dict] = self.parse_option["installedAddons"]
+    def download(self, parser_progress_callback: collections.abc.Callable[[ParserProgress], None] = None) -> None:
+        install_path: str = self.parser_option["installPath"]
+        name: str = self.parser_option["name"]
+        installed_addons: list[dict] = self.parser_option["installedAddons"]
 
-        instance_install_path: str = parse_progress_callback(ParseProgress([
+        instance_install_path: str = parser_progress_callback(ParserProgress([
             f"{install_path}",
             f"{name}"
-        ], ParseState.VALIDATE_INFO_INSTANCE_INSTALL_PATH))
+        ], ParserState.PARSER_INFO_INSTANCE_INSTALL_PATH))
 
-        base_mod_loader_date_modified: str = self.parse_option["baseModLoader"]["dateModified"]
-        base_mod_loader_download_url: str = self.parse_option["baseModLoader"]["downloadUrl"]
-        base_mod_loader_file_name: str = self.parse_option["baseModLoader"]["filename"]
-        base_mod_loader_forge_version: str = self.parse_option["baseModLoader"]["forgeVersion"]
-        base_mod_loader_minecraft_version: str = self.parse_option["baseModLoader"]["minecraftVersion"]
-        base_mod_loader_name: str = self.parse_option["baseModLoader"]["name"]
+        base_mod_loader_date_modified: str = self.parser_option["baseModLoader"]["dateModified"]
+        base_mod_loader_download_url: str = self.parser_option["baseModLoader"]["downloadUrl"]
+        base_mod_loader_file_name: str = self.parser_option["baseModLoader"]["filename"]
+        base_mod_loader_forge_version: str = self.parser_option["baseModLoader"]["forgeVersion"]
+        base_mod_loader_minecraft_version: str = self.parser_option["baseModLoader"]["minecraftVersion"]
+        base_mod_loader_name: str = self.parser_option["baseModLoader"]["name"]
         
         install_process: bool = False
         loader_install_option: str = "Y"
 
         if (not install_process):
-            loader_install_option = parse_progress_callback(ParseProgress([
+            loader_install_option = parser_progress_callback(ParserProgress([
                 f"{base_mod_loader_date_modified}",
                 f"{base_mod_loader_download_url}",
                 f"{base_mod_loader_file_name}",
                 f"{base_mod_loader_forge_version}",
                 f"{base_mod_loader_minecraft_version}",
                 f"{base_mod_loader_name}"
-            ], ParseState.VALIDATE_INFO_LOADER_INSTALL_OPTION))
+            ], ParserState.PARSER_INFO_LOADER_INSTALL_OPTION))
             
             if (loader_install_option == "A"):
                 install_process = True
                 loader_install_option = "Y"
             
             if (loader_install_option == "N"):
-                parse_progress_callback(ParseProgress([
+                parser_progress_callback(ParserProgress([
                     f"{base_mod_loader_date_modified}",
                     f"{base_mod_loader_download_url}",
                     f"{base_mod_loader_file_name}",
                     f"{base_mod_loader_forge_version}",
                     f"{base_mod_loader_minecraft_version}",
                     f"{base_mod_loader_name}"
-                ], ParseState.VALIDATE_WARN_LOADER_INSTALL_SKIP))
+                ], ParserState.PARSER_WARN_LOADER_INSTALL_SKIP))
             elif (loader_install_option == "Y"):
-                parse_progress_callback(ParseProgress([
+                parser_progress_callback(ParserProgress([
                     f"{base_mod_loader_date_modified}",
                     f"{base_mod_loader_download_url}",
                     f"{base_mod_loader_file_name}",
                     f"{base_mod_loader_forge_version}",
                     f"{base_mod_loader_minecraft_version}",
                     f"{base_mod_loader_name}"
-                ], ParseState.VALIDATE_INFO_LOADER_INSTALL_DRAFT))
+                ], ParserState.PARSER_INFO_LOADER_INSTALL_DRAFT))
                 
                 loader_install_path: str = os.path.normpath(instance_install_path)
                 loader_install_file: str = os.path.join(loader_install_path, base_mod_loader_file_name)
@@ -94,7 +94,7 @@ class Parse:
                 except Exception as e:
                     message: str = e.args[0]
 
-                    raise Exception(ParseProgress([
+                    raise Exception(ParserProgress([
                         f"{message}",
                         f"{base_mod_loader_date_modified}",
                         f"{base_mod_loader_download_url}",
@@ -102,7 +102,7 @@ class Parse:
                         f"{base_mod_loader_forge_version}",
                         f"{base_mod_loader_minecraft_version}",
                         f"{base_mod_loader_name}"
-                    ], ParseState.VALIDATE_CRITICAL_LOADER_DOWNLOAD_PROGRESS))
+                    ], ParserState.PARSER_CRITICAL_LOADER_DOWNLOAD_PROGRESS))
 
         for installed_addon in installed_addons:
             installed_file_display_name: str = installed_addon["installedFile"]["displayName"]
@@ -113,33 +113,33 @@ class Parse:
             addon_install_option: str = "Y"
             
             if (not install_process):
-                addon_install_option = parse_progress_callback(ParseProgress([
+                addon_install_option = parser_progress_callback(ParserProgress([
                     f"{installed_file_display_name}",
                     f"{installed_file_download_url}",
                     f"{installed_file_file_date}",
                     f"{installed_file_file_name}"
-                ], ParseState.VALIDATE_INFO_ADDON_INSTALL_OPTION))
+                ], ParserState.PARSER_INFO_ADDON_INSTALL_OPTION))
             
             if (addon_install_option == "A"):
                 install_process = True
                 addon_install_option = "Y"
             
             if (addon_install_option == "N"):
-                parse_progress_callback(ParseProgress([
+                parser_progress_callback(ParserProgress([
                     f"{installed_file_display_name}",
                     f"{installed_file_download_url}",
                     f"{installed_file_file_date}",
                     f"{installed_file_file_name}"
-                ], ParseState.VALIDATE_WARN_ADDON_INSTALL_SKIP))
+                ], ParserState.PARSER_WARN_ADDON_INSTALL_SKIP))
 
                 continue
             elif (addon_install_option == "Y"):
-                parse_progress_callback(ParseProgress([
+                parser_progress_callback(ParserProgress([
                     f"{installed_file_display_name}",
                     f"{installed_file_download_url}",
                     f"{installed_file_file_date}",
                     f"{installed_file_file_name}"
-                ], ParseState.VALIDATE_INFO_ADDON_INSTALL_DRAFT))
+                ], ParserState.PARSER_INFO_ADDON_INSTALL_DRAFT))
 
                 addon_install_path: str = os.path.join(instance_install_path, "mods")
                 addon_install_file: str = os.path.join(addon_install_path, installed_file_file_name)
@@ -150,10 +150,10 @@ class Parse:
                 except Exception as e:
                     message: str = e.args[0]
 
-                    raise Exception(ParseProgress([
+                    raise Exception(ParserProgress([
                         f"{message}",
                         f"{installed_file_display_name}",
                         f"{installed_file_download_url}",
                         f"{installed_file_file_date}",
                         f"{installed_file_file_name}"
-                    ], ParseState.VALIDATE_CRITICAL_ADDON_DOWNLOAD_PROGRESS))
+                    ], ParserState.PARSER_CRITICAL_ADDON_DOWNLOAD_PROGRESS))
